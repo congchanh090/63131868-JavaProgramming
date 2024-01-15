db4062f813c10bd3f7964a473eb80fbb8590379b
congchanh090 <145771857+congchanh090@users.noreply.github.com>
2024-01-16 00:01:54 +0700

    Ví dụ đầu tiên
    Chỉ để demo

---
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello") ; 
+	}
+
+}
