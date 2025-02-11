0bb1200fdb35ad7cf983f7d85603c1a9bb117d89
congchanh090 <145771857+congchanh090@users.noreply.github.com>
2025-02-11 15:14:35 +0700

    LAB1-BAI1

--- BAITAPLAB1/src/lecongchanh/Lab1Bai1.java
+++ BAITAPLAB1/src/lecongchanh/Lab1Bai1.java
@@ -1,8 +1,9 @@
 package lecongchanh;
+
 import java.util.Scanner;
 
 public class Lab1Bai1 {
-
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Họ và tên: ");
@@ -10,7 +11,6 @@ public class Lab1Bai1 {
 		System.out.print("Điểm TB: ");
 		double diemTB = scanner.nextDouble();
         System.out.printf("Họ và tên : %s , Điểm tb :  %f", hoTen, diemTB);
-
 	}
 
 }
