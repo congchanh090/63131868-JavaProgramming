0ea914acd5180142ed9986e761f0a7d162393af2
congchanh090 <145771857+congchanh090@users.noreply.github.com>
2025-02-11 15:40:41 +0700

    LAB2-BAI1

---
+++ BAITAPLAB2/src/lecongchanh/Lab2Bai1.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package lecongchanh;
+
+import java.util.Scanner;
+
+public class Lab2Bai1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		  Scanner scanner = new Scanner(System.in);
+	        //nhap du lieu
+	        
+	        System.out.print("nhap a: ");
+	        double a = scanner.nextDouble();
+	        System.out.print("nhap b : ");
+	        double b = scanner.nextDouble();
+	        
+	        if(a == 0){
+	        	if(b == 0){
+	        	System.out.print("vô số nghiệm") ; 
+	        	}
+	        	else{
+	        	System.out.printf("vô nghiệm ") ; 
+	        	}
+	        }
+	        	else{
+	             double  x = -b/a;
+		        System.out.printf("%.2f" , x) ; 
+
+	        	}
+
+	}
+
+}
